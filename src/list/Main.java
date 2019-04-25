7dfa9417b26a851f062df55ad893c2af9eec6a90
loraine_cit <loraine@ciandt.com>
2019-04-25 13:26:56 -0300

    first commit txt reader file implemented

--- src/list/Main.java
+++ src/list/Main.java
@@ -1,21 +1,23 @@
+package list;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class Main {
-
-    public static void main(String[] args) {
-    
+	
+	public static void main(String[] args) {
+		
 		System.out.printf("\nConteúdo do arquivo texto:\n");
 		try {
 			BufferedReader lerArq = new BufferedReader(new FileReader("./src/exe1.txt"));
-		
+			
 			String linha = lerArq.readLine(); // lê a primeira linha
 			// a variável "linha" recebe o valor "null" quando o processo
 			// de repetição atingir o final do arquivo texto
 			while (linha != null) {
-				System.out.printf("%s\n", linha);
-			
+				//System.out.printf("%s\n", linha);
+				
 				linha = lerArq.readLine(); // lê da segunda até a última linha
 			}
 			
@@ -23,7 +25,7 @@ public class Main {
 			System.err.printf("Erro na abertura do arquivo: %s.\n",
 					e.getMessage());
 		}
-	
+		
 		System.out.println();
-    }
+	}
 }
